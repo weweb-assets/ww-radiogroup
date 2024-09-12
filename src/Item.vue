72effefda5e47d1cd00fa4c3a747caaf5def8564
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-09-13 00:11:26 +0200

    wip

--- src/Item.vue
+++ src/Item.vue
@@ -1,11 +1,11 @@
 <template>
     <wwLayoutItemContext is-repeat :index="index" :data="data">
-        <wwElement v-bind="container" :selected-value="selectedValue" />
+        <wwElement v-bind="container" :selected-value="selectedValue" @update:itemValue="updatedItemValue" />
     </wwLayoutItemContext>
 </template>
 
 <script>
-import { toRef, provide, reactive } from 'vue';
+import { ref, toRef, provide, reactive, computed } from 'vue';
 export default {
     props: {
         container: { type: Object, required: true },
@@ -16,20 +16,27 @@ export default {
     },
     emits: ['update:selectedValue'],
     setup(props, context) {
+        const itemValue = ref(undefined);
+
         function select() {
             if (props.readonly) return;
             context.emit('update:selectedValue', value.value);
         }
 
+        function updatedItemValue(value) {
+            itemValue.value = value;
+        }
+
         const data = reactive({
             item: toRef(props, 'item'),
             value: props.selectedValue,
+            isSelected: computed(() => props.selectedValue === itemValue.value),
             methods: { select },
         });
 
         provide('_wwRadioSelect', select);
 
-        return { select, data };
+        return { select, data, updatedItemValue };
     },
 };
 </script>

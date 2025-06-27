bef5d87367e0ee726a0e4e8bb94b771a13db4bd6
Léo Coletta <mail.leo.coletta@gmail.com>
2025-06-27 11:25:58 +0200

    refactor: use appearance none instead of manual CSS resets
    - Replace manual border/padding/margin resets with appearance: none
    - Cleaner and more maintainable approach
    - Standard way to remove native fieldset styling

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -126,10 +126,8 @@ Access this context in child elements using the key 'radiogroup'
 
 <style scoped>
 .ww-radiogroup {
+    appearance: none;
     display: flex;
-    border: none;
-    padding: 0;
-    margin: 0;
     min-width: 0; /* Fix for fieldset min-width in some browsers */
 }
 

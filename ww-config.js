4d7c49473e7d85f4649a389f2d81a1b0a9e62ac5
MattS-98 <matthewsandys1@gmail.com>
2026-03-20 16:52:17 +0100

    feat: add workflow trigger descriptions
    Add element-specific trigger descriptions used by workflow templates.

--- ww-config.js
+++ ww-config.js
@@ -13,8 +13,8 @@ export default {
     },
     states: ['readonly'],
     triggerEvents: [
-        { name: 'change', label: { en: 'On change' }, event: { value: '' }, default: true },
-        { name: 'initValueChange', label: { en: 'On init value change' }, event: { value: '' } },
+        { name: 'change', label: { en: 'On change' }, event: { value: '' }, default: true, description: "Run when the value changes." },
+        { name: 'initValueChange', label: { en: 'On init value change' }, event: { value: '' }, description: "Run when the initial value changes." },
     ],
     properties: {
         items: {
